c94cf489d98e8c5e3b304eed99a665375310094c
MistyIslam5 <mdmisty5@gmail.com>
2024-10-01 08:38:53 +0600

    api learning

---
+++ js/json.js
@@ -0,0 +1,14 @@
+// const user = {id : 1, name : 'misty islam', job : 'actor'}
+// const stringify = JSON.stringify(user)
+
+// console.log(user)
+// console.log(stringify)
+
+
+
+function loadData (){
+    const url = 'https://jsonplaceholder.typicode.com/todos/1'
+fetch(url)
+.then(Response => Response.json())
+.then(json => console.log(json))
+}
